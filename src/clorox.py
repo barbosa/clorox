41fbeec795f2058a6f76c91d80d9a45e2d60e641
Gustavo Barbosa <gustavocsb@gmail.com>
2016-02-07 13:43:43 -0500

    separate responsiblities

--- src/clorox.py
+++ src/clorox.py
@@ -1,30 +1,8 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
-import sys, os, re
+import sys, os
 import getopt
-
-
-class Matcher:
-
-    HEADER_TEMPLATE = (r""
-        "\/\/\n"
-        "\/\/.*\..*\n"
-        "\/\/.*\n"
-        "\/\/\n"
-        "\/\/\s{2}Created by\s.*\son\s\d{1,2}\/\d{1,2}\/\d{2}\.\n"
-        "\/\/\s{2}Copyright\s(\(c\)|©)\s\d{4}\s.*\.\sAll rights reserved\.\n"
-        "\/\/\n"
-    )
-
-    def __init__(self, header):
-        self.header = header
-
-    def matches(self):
-        return re.match(self.HEADER_TEMPLATE, self.header) is not None
-
-
-class Printer:
-    pass # TODO
+from matcher import Matcher
 
 
 class Clorox:
---
+++ src/matcher.py
@@ -0,0 +1,21 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+import re
+
+class Matcher:
+
+    HEADER_TEMPLATE = (r""
+        "\/\/\n"
+        "\/\/.*\..*\n"
+        "\/\/.*\n"
+        "\/\/\n"
+        "\/\/\s{2}Created by\s.*\son\s\d{1,2}\/\d{1,2}\/\d{2}\.\n"
+        "\/\/\s{2}Copyright\s(\(c\)|©)\s\d{4}\s.*\.\sAll rights reserved\.\n"
+        "\/\/\n"
+    )
+
+    def __init__(self, header):
+        self.header = header
+
+    def matches(self):
+        return re.match(self.HEADER_TEMPLATE, self.header) is not None
